a1595327ec6c1e03db6943e20fec011c38fe51ef
Justin Taft <jjtaft@gmail.com>
2017-05-01 21:02:28 -0500

    Add files via upload

--- Bamazon.js
+++ Bamazon.js
@@ -1,4 +1,3 @@
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
@@ -6,34 +5,50 @@ var config = require('./config.js');
 var dbConnection = mysql.createConnection(config);
 
 dbConnection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
 
 connection.query("SELECT * FROM products", function(err, res) {
-  if (err) throw err;
-  console.log(res);
+    if (err) throw err;
+    console.log(res);
 });
 
 var purchaseItem = function() {
 
-    inquirer.prompt([{
-        type: "input",
-        name: "idRequest",
-        message: "Please enter the product ID of the item you wish to purchase"
-    }, {
-        type: "input",
-        name: "amountRequest",
-        message: "How many would you like to purchase?"
+        inquirer.prompt([{
+                type: "input",
+                name: "idRequest",
+                message: "Please enter the product ID of the item you wish to purchase"
+            }, {
+                type: "input",
+                name: "amountRequest",
+                message: "How many would you like to purchase?"
+            }]).then(function(answers) {
 
-    }]).then(function(answers) {
+                    var chosenItem;
+                    for (var i = 0; i < results.length; i++) {
+                        if (results[i].item_id == answer.item_selection) {
+                            chosenItem = results[i];
+                        }
+                    }
 
-    connection.query("INSERT INTO items SET ?", {
-	type: answers.type,
-	name: answers.name,
-	bid: answers.bid
-}, function(err, res) {});
-    });
+                    if (chosenItem.stock_quantity < parseInt(answer.amountRequest) || chosenItem.stock_quantity == 0) {
+                        console.log("Insufficient quantity! There's only " + chosenItem.stock_quantity + " available. Please try again.");
+                        userSelectProduct();
+                    } else {
 
-    if (err) throw err;
-	console.log("Item has been listed");
+                        var newStockAmount = chosenItem.stock_quantity - answer.units;
+
+                        connection.query("UPDATE products SET ? WHERE ?", [{
+
+                            stock_quantity: newStockAmount
+
+                        }, {
+
+                            item_id: chosenItem.item_id
+                        }], function(error) {
+                            if (error) throw err;
+                            console.log("Stock Updated");
+
+                        });
